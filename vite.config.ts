4804a705506f4cdf2ee11fac28184911ce44c528
mdhruvil <dhruvil1808@gmail.com>
2025-09-06 22:07:03 +0530

    feat: make it embeddable

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+    headers: {
+      "Content-Security-Policy":
+        "frame-ancestors 'self' http://localhost:3000 https://vibe.appwrite.network;",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Cross-Origin-Embedder-Policy": "credentialless",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+    },
   },
 });
