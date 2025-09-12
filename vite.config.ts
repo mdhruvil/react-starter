03807a98896db087ddf795785065a3c27d93a9e4
mdhruvil <dhruvil1808@gmail.com>
2025-09-12 23:58:57 +0530

    feat: add a new domain

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     host: "0.0.0.0",
     headers: {
       "Content-Security-Policy":
-        "frame-ancestors 'self' http://localhost:3000 https://vibe.appwrite.network;",
+        "frame-ancestors 'self' http://localhost:3000 https://vibe.appwrite.network https://vibe.mdhruvil.page;",
       "Referrer-Policy": "strict-origin-when-cross-origin",
       "Cross-Origin-Embedder-Policy": "credentialless",
       "Cross-Origin-Resource-Policy": "cross-origin",
